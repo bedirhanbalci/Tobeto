ed0063787242762679302b51369d882485b71d06
Bedirhan Balcı <bedirhanbalci@outlook.com>
2023-12-22 19:07:25 +0300

    Javascript Basic

---
+++ JavaScript/LessonsContents/JS-05/script.js
@@ -0,0 +1,18 @@
+var name = "Bedirhan Balci";
+
+function showName() {
+  console.log(name);
+}
+
+function sum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function writeText(text) {
+  console.log("writeText method worked");
+  document.write(text);
+}
+
+function bodyOnLoad() {
+  console.log("body loaded");
+}
